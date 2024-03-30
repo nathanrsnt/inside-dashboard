7b9751ee3288375d17105ffb0e216b7ecadd3f71
myazuk <glavoguawa@gmail.com>
2024-03-30 15:37:53 -0300

    socket connection btween php and cpp

--- app/Http/Controllers/ScanController.php
+++ app/Http/Controllers/ScanController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 
 class ScanController extends Controller
 {
@@ -16,48 +15,66 @@ class ScanController extends Controller
         $ipAddresses = explode(';', $request->input('ip'));
         $args = explode(' ', $request->input('checkedValues'));
         $allArgs = array('-p-', '-P', '-sV', '-T5', '-A', '-iR', '-sn', '-v');
-        $serverAddress = "127.0.0.1";
-        $serverPort = 27908;
+
+        $address = "127.0.0.1";
+        $port = 27908;
         $result = [];
         $validArgs = '';
 
+        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
+        if (!$socket) {
+            return response()->json(['error' => "Failed to create socket"], 500);
+        }
+
+        if (!socket_connect($socket, $address, $port)) {
+            return response()->json(['error' => "Failed to connect to server"], 500);
+        }
+
         foreach ($args as $arg) {
             if (!in_array($arg, $allArgs)) {
-                return response()->json(['error' => "Invalid arguments!"], 400);
+                return response()->json(['error' => "Invalid arguments!"], 500);
             } else {
                 $validArgs .= $arg . ' ';
             }
         }
 
-        $wrappedArgs = '"'.trim($validArgs,'"').'"';
-
-
         foreach ($ipAddresses as $ip) {
             $command = [
-                'action' => 'nmap_scan',
+                'action' => 'nmap-scan',
                 'ip' => $ip,
-                'args' => $wrappedArgs
+                'args' => $validArgs
             ];
 
             try {
                 // Send JSON data to the server
-                $response = Http::post("http://{$serverAddress}:{$serverPort}", $command);
+                $jsonCommand = json_encode($command);
+
+                $bytesWritten = @socket_write($socket, $jsonCommand, strlen($jsonCommand));
+                
+                if ($bytesWritten === false) {
+                    $errorCode = socket_last_error();
+                    $errorMessage = socket_strerror($errorCode);
+                    throw new \Exception("Failed to write to socket: [$errorCode] $errorMessage");
+                }
+                if ($bytesWritten < strlen($jsonCommand)) {
+                    throw new \Exception("Incomplete write to socket. Only $bytesWritten bytes written out of " . strlen($jsonCommand));
+                }
+
+                $response = socket_read($socket, 1024);
             } catch (\Exception $e) {
                 return response()->json([
                     'error' => $e->getMessage(),
                 ], 500);
             }
 
-            if ($response->successful()) {
-                $responseData = $response->body();
-            }
-
             $result[] = [
                 'ip' => $ip,
-                'output' => $responseData
+                'output' => $response
             ];
         }
 
+        socket_close($socket);
         return response()->json($result);
     }
 
@@ -65,9 +82,6 @@ class ScanController extends Controller
         return view('scans.gobuster');
     }
 
-    public function runGosbuter(Request $request) {
-
-        
-        
+    public function runGosbuter(Request $request) { 
     }
 }
